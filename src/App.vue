7efbbb9f11498e23fb4255dc0e916888f55526fd
HellR@y <hellraygh@gmail.com>
2024-02-09 23:58:15 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import navHead from './components/navHead.vue'
+import contentBlock from './components/contentBlock.vue'
+</script>
+
+<template>
+<div class="wrapper">
+  <navHead/>
+  <contentBlock/>
+</div>
+
+
+</template>
+
+
---
+++ src/components/navHead.vue
@@ -0,0 +1,28 @@
+<template>
+  
+      <header>
+        <div class="left">
+          <img class="logo" src="/logo.png" alt="Logo"/>
+          <div class="title">
+            <h2>Vue Sneakers</h2>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+  
+        <ul>
+          <li>
+            <img src="/cart.svg" alt="cart">  
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="/heart.svg" alt="heart">
+            <span>Закладки</span>
+          </li>
+          <li>
+            <img src="/profile.svg" alt="profile">
+            <span>Профиль</span>
+          </li>
+        </ul>
+      </header>
+  
+  </template>
